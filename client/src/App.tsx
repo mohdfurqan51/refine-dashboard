ae9b01c4fc69d1b1f619af3295b7f58851185fb4
Mohd Furqan <88854140+mohdfurqan51@users.noreply.github.com>
2023-05-06 04:39:35 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 
       // Save user to MongoDB
       if(profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://refine-dashboard-i09e.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-type': 'application/json' },
           body: JSON.stringify({
@@ -128,7 +128,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-dashboard-i09e.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
