fa9db1eaaf2403d3050ad78dacc730e648dfe771
Mohd Furqan <88854140+mohdfurqan51@users.noreply.github.com>
2023-05-07 00:15:41 +0530

    Update login.tsx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -26,12 +26,14 @@ export const Login: React.FC = () => {
       }
 
       try {
+        
         window.google.accounts.id.initialize({
           ux_mode: "popup",
           client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
-            setLoading(true);
+            
             if (res.credential) {
+              setLoading(true);
               login(res);
               setLoading(false);
             }
